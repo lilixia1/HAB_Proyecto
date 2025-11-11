f80c4ea0156cd5defb1fe4c29aeb7b7ae65460ea
lilixia1 <lilixia13@uma.es>
2025-11-11 15:05:18 +0100

    Corregido script descargar_ruta.py para que cargue en carpeta data

--- scripts/descargar_ruta.py
+++ scripts/descargar_ruta.py
@@ -1,4 +1,5 @@
 import gseapy as gp
+import os
 
 # 1. Elige la librería de rutas
 # Puedes probar con: "KEGG_2021_Human", "Reactome_2022", "KEGG_2019_Human", etc.
@@ -26,10 +27,21 @@ print(f"Ruta encontrada: {ruta_encontrada}")
 print(f"Número de genes: {len(genes_ruta)}")
 
 # 4. Guardar a un txt (un gen por línea)
+# Ruta base del proyecto (un nivel por encima del script actual)
+basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Carpeta donde quieres guardar los datos
+datadir = os.path.join(basedir, "data")
+
+# Crear la carpeta si no existe
+os.makedirs(datadir, exist_ok=True)
+
 output_file = "genes_ruta.txt"
+output_path = os.path.join(datadir, output_file)
+
 with open(output_file, "w") as f:
     for g in genes_ruta:
         f.write(g + "\n")
 
-print(f"Genes guardados en: {output_file}")
+print(f"Genes guardados en: data/{output_file}")
 
